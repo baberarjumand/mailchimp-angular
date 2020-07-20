3130d5b3ade000f6354c07f5d634e3310fe04377
baberarjumand <baber.arjumand@gmail.com>
2020-07-20 14:58:52 +0300

    implemented and tested first version of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+interface MailChimpResponse {
+  result: string;
+  msg: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mailchimp-angular-v1';
+  sampleForm: FormGroup;
+  validateEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  mailChimpEndpoint: string;
+  endpointPlaceholder =
+    'https://gmail.us10.list-manage.com/subscribe/post-json?u=********&amp;id=*********';
+  httpResponse = {
+    msg: 'No Request Sent Yet',
+  };
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.sampleForm = this.formBuilder.group({
+      endpoint: ['', Validators.required],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.validateEmailRegex),
+        ]),
+      ],
+      firstName: [''],
+      lastName: [''],
+    });
+  }
+
+  onSubmit() {
+    // console.log(this.sampleForm.value);
+
+    this.mailChimpEndpoint = this.sampleForm.value.endpoint + '&';
+
+    const params = new HttpParams()
+      .set('FNAME', this.sampleForm.value.firstName)
+      .set('LNAME', this.sampleForm.value.lastName)
+      .set('EMAIL', this.sampleForm.value.email)
+      .set('b_123abc123abc123abc123abc123abc123abc', ''); // hidden input name
+
+    const mailChimpReqUrl = this.mailChimpEndpoint + params.toString();
+
+    this.http.jsonp<MailChimpResponse>(mailChimpReqUrl, 'c').subscribe(
+      (response) => {
+        console.log('HTTP Request successful!');
+        console.log(response);
+        this.httpResponse = response;
+      },
+      (error) => {
+        console.log('HTTP Request Errored!');
+        console.log(error);
+        this.httpResponse = error;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
